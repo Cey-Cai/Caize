e096f28312495d3d394ebea7ba2932b5de5b66c6
Cey-Cai <1600973669@qq.com>
2023-12-25 22:16:25 +0800

    12/25

--- src/View/AdminLibrary/ABook.java
+++ src/View/AdminLibrary/ABook.java
@@ -12,11 +12,21 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ABook extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 	private JTable table;
 
 	/**
@@ -41,7 +51,7 @@ public class ABook extends JFrame {
 	public ABook() {
 		setFont(new Font("Dialog", Font.PLAIN, 30));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 679, 564);
+		setBounds(100, 100, 855, 731);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(192, 192, 192));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -50,31 +60,82 @@ public class ABook extends JFrame {
 		contentPane.setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 10, 585, 385);
+		tabbedPane.setBounds(10, 10, 761, 546);
 		tabbedPane.setFont(new Font("宋体", Font.PLAIN, 20));
 		contentPane.add(tabbedPane);
 		
-		JPanel panel = new JPanel();
-		panel.setToolTipText("");
-		tabbedPane.addTab("\u4E66\u7C4D\u67E5\u770B", null, panel, null);
-		panel.setLayout(null);
-		
-		table = new JTable();
-		table.setBounds(290, 139, 0, 0);
-		panel.add(table);
-		tabbedPane.setEnabledAt(0, true);
-		
-		JPanel panel_2 = new JPanel();
-		tabbedPane.addTab("\u4E66\u7C4D\u5220\u9664", null, panel_2, null);
-		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("\u4E66\u7C4D\u6DFB\u52A0", null, panel_1, null);
+		panel_1.setLayout(null);
 		
-		JPanel panel_3 = new JPanel();
-		tabbedPane.addTab("\u4E66\u7C4D\u4FEE\u6539", null, panel_3, null);
+		textField_1 = new JTextField();
+		textField_1.setBounds(161, 44, 211, 51);
+		panel_1.add(textField_1);
+		textField_1.setColumns(1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(1);
+		textField_2.setBounds(161, 122, 211, 52);
+		panel_1.add(textField_2);
+		
+		JLabel lblNewLabel = new JLabel("\u4E66\u540D");
+		lblNewLabel.setFont(new Font("宋体", Font.PLAIN, 30));
+		lblNewLabel.setBounds(26, 39, 81, 51);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("\u4E66\u7C4D\u7F16\u53F7");
+		lblNewLabel_1.setFont(new Font("宋体", Font.PLAIN, 30));
+		lblNewLabel_1.setBounds(26, 122, 125, 51);
+		panel_1.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("\u4F5C\u8005");
+		lblNewLabel_2.setFont(new Font("宋体", Font.PLAIN, 30));
+		lblNewLabel_2.setBounds(26, 199, 81, 51);
+		panel_1.add(lblNewLabel_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(1);
+		textField_3.setBounds(161, 204, 211, 51);
+		panel_1.add(textField_3);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setToolTipText("");
+		tabbedPane.addTab("\u4E66\u7C4D增删", null, panel_2, null);
+		panel_2.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		scrollPane.setBounds(66, 47, 429, 365);
+		panel_2.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Name", "Num", "Writer", "Set"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, true, true, true
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		scrollPane.setViewportView(table);
+		
+		JLabel lblNewLabel_3 = new JLabel("\u53EF\u76F4\u63A5\u5728\u8868\u8FDB\u884C\u64CD\u4F5C");
+		lblNewLabel_3.setFont(new Font("宋体", Font.PLAIN, 20));
+		lblNewLabel_3.setBounds(66, 10, 382, 27);
+		panel_2.add(lblNewLabel_3);
 		
 		JButton btnNewButton = new JButton("\u8FD4\u56DE");
-		btnNewButton.setBounds(234, 421, 151, 59);
+		btnNewButton.setBounds(349, 589, 151, 59);
 		btnNewButton.setBackground(new Color(255, 128, 64));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -90,5 +151,4 @@ public class ABook extends JFrame {
 		btnNewButton.setFont(new Font("宋体", Font.PLAIN, 20));
 		contentPane.add(btnNewButton);
 	}
-
 }
